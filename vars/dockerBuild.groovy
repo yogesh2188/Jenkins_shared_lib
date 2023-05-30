0e80a445303fbc14f29750cc2a194945da254e25
RutujaPawal <rutujapawal177@gmail.com>
2023-05-30 15:14:42 +0530

    docker build

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,12 +1,3 @@
-// def call(String project, String ImageTag, String hubUser){
-    
-//     sh """
-//      docker image build -t ${hubUser}/${project} . 
-//      docker image tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}
-//      docker image tag ${hubUser}/${project} ${hubUser}/${project}:latest
-//     """
-// }
-
 def call(String aws_account_id, String region, String ecr_repoName){
     
     sh """
