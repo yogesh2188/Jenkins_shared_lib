4a2d2138e2f0d684569cf0f0d798ab975f463bd2
yogesh2188 <111947381+yogesh2188@users.noreply.github.com>
2023-06-08 01:02:42 +0530

    Update QualityGateStatus.groovy

--- vars/QualityGateStatus.groovy
+++ vars/QualityGateStatus.groovy
@@ -2,9 +2,7 @@
 
 def call(credentialsId){
   
-  waitForQualityGate abortPipeline: false, credentialsId: 'sonarqube-Api'
-
-//waitForQualityGate abortPipeline: false, credentialsId: credentialsId
+waitForQualityGate abortPipeline: false, credentialsId: credentialsId
 
 }
 
